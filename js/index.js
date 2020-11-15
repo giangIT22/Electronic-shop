2240d53e8db68ad4942a9d8d2b8d7ea7b1646f4d
giang <giang22.fit@gmail.com>
2020-11-15 15:00:57 +0700

    add all

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
         pagination: false,
         autoPlay: true,
     });
+    
     $("#testimonial-slider-2").owlCarousel({
         items: 1,
         itemsDesktop: [1000, 1],
@@ -16,5 +17,15 @@ $(document).ready(function() {
         itemsMobile: [480, 1],
         pagination: true,
         autoPlay: false,
+    });
+
+    $("#testimonial-slider-3").owlCarousel({
+        items: 4,
+        itemsDesktop: [1000, 4],
+        itemsDesktopSmall: [979, 3],
+        itemsTablet: [768, 3],
+        itemsMobile: [480, 1],
+        pagination: false,
+        autoPlay: true,
     });
 });
